a5c8b4bdd4268ba6d4a5c48a19746f3a8a12fef4
Dutarega135 <dutarega595@gmail.com>
2023-05-23 20:23:09 +0700

    first commit

---
+++ component/data.js
@@ -0,0 +1,69 @@
+import { FontAwesome5 } from '@expo/vector-icons';
+
+const DataJadwalPenerbangan = [
+  [
+    "Medan",
+    "Jawa Tengah",
+    "Sulawesi Selatan",
+    "Banjarmasin",
+    "Padang",
+    "NTB",
+    "Riau",
+    "Jakarta",
+    "Jawa Barat",
+    "Palembang",
+  ],
+  [
+    "Balikpapan",
+    "Malang",
+    "Bandar Lampung",
+    "Jambi",
+    "Nias",
+    "Medan",
+    "Bali",
+    "Lombok",
+    "Riauh9",
+    "Aceh",
+  ],
+  [
+    "11 April 2023",
+    "11 April 2023",
+    "11 April 2023",
+    "12 April 2023",
+    "12 April 2023",
+    "12 April 2023",
+    "13 April 2023",
+    "13 April 2023",
+    "13 April 2023",
+    "13 April 2023"
+
+  ],
+  [
+    "Batik Air",
+    "Sriwijaya Air",
+    "Sriwijaya Air",
+    "Elang Air",
+    "Lion Air",
+    "Sriwijaya Air",
+    "Batik Air",
+    "Lion Air",
+    "Lion Air",
+    "Elang Air",
+  ],
+  [
+    <FontAwesome5 name="plane-arrival" size={24} color="blue" />,
+    <FontAwesome5 name="plane-departure" size={25} color="green" />,
+    <FontAwesome5 name="plane-departure" size={25} color="green" />,
+    <FontAwesome5 name="plane-arrival" size={24} color="red" />,
+    <FontAwesome5 name="plane" size={24} color="red" />,
+    <FontAwesome5 name="plane-departure" size={25} color="green" />,
+    <FontAwesome5 name="plane-arrival" size={24} color="blue" />,
+    <FontAwesome5 name="plane" size={24} color="red" />,
+    <FontAwesome5 name="plane" size={24} color="red" />,
+    <FontAwesome5 name="plane-arrival" size={24} color="red" />
+    
+  ]
+];
+
+
+export default DataJadwalPenerbangan;
